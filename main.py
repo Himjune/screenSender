330e0d13894717a6954b5e5c039aa9e4d1dd51cc
Himjune <himjune@mail.ru>
2020-11-06 21:16:09 +0300

    done tornado w/o ngginx

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ import PIL.ImageGrab
 from ts_collector import TsCollector
 from server import wsServer
 
+import tornado.ioloop
+import tornado.web
+
+import hashlib
+
 class ScreenCapturer:
     def __init__(self):
         self.buffers = [{},{}]
@@ -25,29 +30,20 @@ class ScreenCapturer:
         self.writeTo = 0
         self.working = True
         self.buf = bytearray(1048576)
+        self.data = io.BytesIO()
+        self.to_send = bytes()
 
         self.tser = TsCollector()
 
-    def update(self, sender):
+    def update(self):
         while(self.working):
             self.tser.start()
-            img = {}
-            #self.buffers[self.writeTo] = pyautogui.screenshot()
-            #img = pyautogui.screenshot()
-            img = PIL.ImageGrab.grab()
-            '''with mss.mss() as sct:
-                # Get rid of the first, as it represents the "All in One" monitor:
-                for num, monitor in enumerate(sct.monitors[1:], 1):
-                    # Get raw pixels from the screen
-                    sct_img = sct.grab(monitor)
-
-                    # Create the Image
-                    img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")'''
+            
+            self.buffers[self.writeTo] = pyautogui.screenshot()
             self.tser.ts('got_img')
 
             data = io.BytesIO()
-            #self.buffers[self.writeTo].save(data, 'JPEG', quality=70)
-            img.save(data, 'JPEG', quality=10)
+            self.buffers[self.writeTo].save(data, 'JPEG', quality=90)
             data.seek(0)
             self.tser.ts('got_img_to_buf')
 
@@ -61,32 +57,62 @@ class ScreenCapturer:
             data = data[2:-1]
             self.tser.ts('got_base64')
 
-            #print(self.tser.stats())
-            sender.to_clients(data, self.tser)
+            self.to_send = data
+
+            #print('got:', abs(hash(self.to_send)) % (10 ** 8))
+
+            self.tser.ts('red_img_to_send')
+
+            if (self.tser.tick[0] % 100 == 0):
+                print(self.tser)
             
     def get_buff(self):
-        readFrom = 1-self.writeTo
-        return self.buffers[readFrom]
+
+        return self.to_send
+
+        #readFrom = 1-self.writeTo
+        #return self.buffers[readFrom]
 
     def stop(self):
         self.working = False
 
-if __name__ == "__main__":
-    scp = ScreenCapturer()
-    server = wsServer()
+scp = ScreenCapturer() 
+class MainHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        #self.set_header("Cache-control", "no-cache")
 
-    screenThread = threading.Thread(target=scp.update, args=(server,))
+    def get(self):
+        self.set_header("Content-Type", "text/plain")
+        #print(scp.get_buff())
+        #print('send:', abs(hash(scp.to_send)) % (10 ** 8))
+        
+        self.write(scp.to_send)
+
+    def options(self):
+        # no body
+        self.set_status(204)
+        self.finish()
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    screenThread = threading.Thread(target=scp.update)
     screenThread.start()
 
-    serverThread = threading.Thread(target=server.listen)
-    serverThread.start()
+    #serverThread = threading.Thread(target=server)
+    #serverThread.start()
 
     working = True
     try:
-        while (working):
-            #print('avg',scp.avg,'\tn',scp.idx, '\twt:', scp.writeTo, end='\r')
-            #server.to_clients(f'Tick {scp.idx} with avg \t{scp.avg}', 0)
-            time.sleep(5)
+        app = make_app()
+        app.listen(8888)
+        tornado.ioloop.IOLoop.current().start()
 
     except (KeyboardInterrupt, SystemExit):
         scp.stop()
