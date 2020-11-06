2f4a115bb944f837cad717017d54ef99d2bdbf5a
Himjune <himjune@mail.ru>
2020-11-06 21:56:59 +0300

    client adapted to any url

--- client/script.js
+++ client/script.js
@@ -3,10 +3,20 @@
 hashCode = function(s){
     return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
 }
+
+
+let domain = document.URL;
+console.log(domain);
+domain = domain.split('/')[2]
+console.log(domain);
+domain = domain.split('/')
+console.log(domain);
+let link = 'http://'+domain+'/?ts='
+
 window.onload = function() {
     function updateImage() {
         let now = new Date()
-        fetch('http://192.168.1.2:8888/?ts='+ Math.round(now.getTime()))
+        fetch(link + Math.round(now.getTime()))
         .then((response) => {
             return response.text();
         })
